c71868aa33c87833c358d056613bab7ec98d6337
beryu <ryutakibe@gmail.com>
2024-03-24 16:07:31 +0900

    Add a base Xcode project

---
+++ window-switcher-mac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  window-switcher-mac
+//
+//  Created by 岐部龍太 on 2024/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ window-switcher-mac/window_switcher_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  window_switcher_macApp.swift
+//  window-switcher-mac
+//
+//  Created by 岐部龍太 on 2024/03/24.
+//
+
+import SwiftUI
+
+@main
+struct window_switcher_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
