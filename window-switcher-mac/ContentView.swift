e155801464b116f5f8ae936b2fc01749d013828a
Ryuta Kibe <ryutakibe@gmail.com>
2024-03-25 13:17:37 +0900

    Add numbers to keys

--- window-switcher-mac/ContentView.swift
+++ window-switcher-mac/ContentView.swift
@@ -49,6 +49,16 @@ struct ContentView: View {
     "x",
     "y",
     "z",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
   ]
 
   var body: some View {
